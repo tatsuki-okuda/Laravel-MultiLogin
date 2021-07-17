d245852b24baef344e8d092bed83f9da8ec048c6
tatasuki-okuda <67616674+tatsuki-okuda@users.noreply.github.com>
2021-07-17 11:54:03 +0900

    pagination add

--- resources/views/admin/owners/index.blade.php
+++ resources/views/admin/owners/index.blade.php
@@ -49,6 +49,8 @@
                             @endforeach
                             </tbody>
                         </table>
+
+                        {{ $owners->links() }}
                         </div>
                     </div>
                     </section>
