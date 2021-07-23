4ebafaa12c9f252ae6f32e352a0befc32a32ab5f
tatasuki-okuda <67616674+tatsuki-okuda@users.noreply.github.com>
2021-07-23 11:24:29 +0900

    images seeder add

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
             OwnerSeeder::class,
             AdminSeeder::class,
             ShopSeeder::class,
+            ImageSeeder::class,
         ]);
     }
 }
