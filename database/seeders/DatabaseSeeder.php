5d45912139650084b3cc9236caca973f9cbd552a
tatasuki-okuda <67616674+tatsuki-okuda@users.noreply.github.com>
2021-07-23 11:57:11 +0900

    add categories and seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,7 @@ class DatabaseSeeder extends Seeder
             AdminSeeder::class,
             ShopSeeder::class,
             ImageSeeder::class,
+            CategorySeeder::class,
         ]);
     }
 }
