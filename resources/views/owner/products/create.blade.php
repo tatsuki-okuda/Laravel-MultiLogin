d3f21555c08b9992e6cd3f5d8123b31b47a2b020
tatasuki-okuda <67616674+tatsuki-okuda@users.noreply.github.com>
2021-07-28 20:32:23 +0900

    画像選択　修正

--- resources/views/owner/products/create.blade.php
+++ resources/views/owner/products/create.blade.php
@@ -26,6 +26,9 @@
                                 </div>
                             </div>
                             <x-select-image :images="$images" name="image1"></x-select-image>
+                            <x-select-image :images="$images" name="image2"></x-select-image>
+                            <x-select-image :images="$images" name="image3"></x-select-image>
+                            <x-select-image :images="$images" name="image4"></x-select-image>
                             <div class="p-2 w-full flex justify-around mt-4">
                                 <button type="button" onclick="location.href='{{ route('owner.products.index')}}'" class="bg-gray-200 border-0 py-2 px-8 focus:outline-none hover:bg-gray-400 rounded text-lg">戻る</button>
                                 <button type="submit" class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">登録する</button>                        
