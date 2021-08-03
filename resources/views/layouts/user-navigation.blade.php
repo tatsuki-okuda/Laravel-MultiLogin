6e21f51cb7e2d40da02897506496e54f1dff33b2
tatasuki-okuda <67616674+tatsuki-okuda@users.noreply.github.com>
2021-08-04 08:53:45 +0900

    items index view　調整

--- resources/views/layouts/user-navigation.blade.php
+++ resources/views/layouts/user-navigation.blade.php
@@ -6,7 +6,7 @@
                 <!-- Logo -->
                 <div class="flex-shrink-0 flex items-center">
                     <div class="w-16">
-                        <a href="{{ route('user.dashboard') }}">
+                        <a href="{{ route('user.items.index') }}">
                             <x-application-logo class="block h-10 w-auto fill-current text-gray-600" />
                         </a>
                     </div>
@@ -14,7 +14,7 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('user.dashboard')" :active="request()->routeIs('user.dashboard')">
+                    <x-nav-link :href="route('user.items.index')" :active="request()->routeIs('user.items.index')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
@@ -65,7 +65,7 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('user.dashboard')" :active="request()->routeIs('user.dashboard')">
+            <x-responsive-nav-link :href="route('user.items.index')" :active="request()->routeIs('user.items.index')">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
         </div>
