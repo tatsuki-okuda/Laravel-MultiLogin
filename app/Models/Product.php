37ba0d761cb3db4ff1be90459b2cfa4a5c256b84
tatasuki-okuda <67616674+tatsuki-okuda@users.noreply.github.com>
2021-08-05 21:57:14 +0900

    cart relation add

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -68,4 +68,10 @@ class Product extends Model
     {
         return $this->hasMany(Stock::class);
     }
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'carts')
+        ->withPivot(['id', 'quantity']);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -40,4 +40,11 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+    public function products()
+    {
+        return $this->belongsToMany(Product::class, 'carts')
+            ->withPivot(['id', 'quantity']);
+    }
 }
