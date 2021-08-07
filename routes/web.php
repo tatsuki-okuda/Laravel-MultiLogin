b05f9cfb20c97eebb0ce4fff1cbec45d5fb3b4af
tatasuki-okuda <67616674+tatsuki-okuda@users.noreply.github.com>
2021-08-07 11:49:26 +0900

    cart delete

--- routes/web.php
+++ routes/web.php
@@ -35,6 +35,7 @@ Route::prefix('cart')
     ->group(function(){
         Route::get('/', [CartController::class, 'index'])->name('cart.index');
         Route::post('add', [CartController::class, 'add'])->name('cart.add');
+        Route::post('delete/{item}', [CartController::class, 'delete'])->name('cart.delete');
     });
 
 // Route::get('/dashboard', function () {
