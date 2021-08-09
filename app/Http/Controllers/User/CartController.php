d04d873c5dfa26eb768b2552ffc49913ee2ffa67
tatasuki-okuda <67616674+tatsuki-okuda@users.noreply.github.com>
2021-08-09 11:16:58 +0900

    strip controller

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -56,4 +56,43 @@ class CartController extends Controller
 
         return redirect()->route('user.cart.index');
     }
+
+
+
+    public function checkout()
+    {
+        $user = User::findOrFail(Auth::id());
+        $products = $user->products;
+
+        /**
+         * https://stripe.com/docs/api/checkout/sessions/create#create_checkout_session-line_items
+         */
+        $lineItems = array();
+        foreach($products as $product){
+            $linItem = [
+                'name' => $product->name,
+                'description' => $product->information,
+                'amount' => $product->price,
+                'currency' => 'jpy',
+                'quantity'  => $product->pivot->quantity
+            ];
+            array_push($lineItems, $linItem);
+        }
+        // dd($lineItems);
+
+        // https://stripe.com/docs/checkout/integration-builder
+        \Stripe\Stripe::setApiKey(env('STRIPE_SECRET_KEY'));
+
+        $session = \Stripe\Checkout\Session::create([
+            'payment_method_types' => ['card',],
+            'line_items' => [$lineItems],
+            'mode' => 'payment',
+            'success_url' => route('user.items.index'),
+            'cancel_url' => route('user.cart.index'),
+        ]);
+
+        $publicKey = env('STRIPE_PUBLIC_KEY');
+
+        return view('user.checkout', compact('session', 'publicKey'));
+    }
 }
