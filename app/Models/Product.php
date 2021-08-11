824795f05527c1fa74e1ee38e589e6f07487eb1a
tatasuki-okuda <67616674+tatsuki-okuda@users.noreply.github.com>
2021-08-11 09:16:05 +0900

    localscope add

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Product extends Model
 {
@@ -73,5 +74,43 @@ class Product extends Model
     {
         return $this->belongsToMany(User::class, 'carts')
         ->withPivot(['id', 'quantity']);
+    }
+
+
+    /**
+     * https://readouble.com/laravel/8.x/ja/eloquent.html#:~:text=SecondScope%3A%3Aclass%0A%5D)-%3Eget()%3B-,%E3%83%AD%E3%83%BC%E3%82%AB%E3%83%AB%E3%82%B9%E3%82%B3%E3%83%BC%E3%83%97,-%E3%83%AD%E3%83%BC%E3%82%AB%E3%83%AB%E3%82%B9%E3%82%B3%E3%83%BC%E3%83%97%E3%82%92
+     *
+     * @param [type] $query
+     * @return void
+     */
+    public function scopeAvailableItems($query)
+    {
+
+        $stocks = DB::table('t_stocks')
+            ->select('product_id', DB::raw('sum(quantity) as quantity')) 
+            ->groupBy('product_id')
+            ->having('quantity', '>', 1);
+
+        // 必ずqueryを返す必要がある。
+        return $query
+            ->joinSub($stocks, 'stock', function($join){
+                $join->on('products.id', '=', 'stock.product_id');
+            })
+            ->join('shops', 'products.shop_id', '=', 'shops.id')
+            // eloquantのリレーションが使えなくなるので、関連するテーブルを紐づける必要がある
+            ->join('secondary_categories', 'products.secondary_category_id', '=','secondary_categories.id')
+            // 同じ名前が使えないので番号をふる
+            ->join('images as image1', 'products.image1', '=', 'image1.id')
+            ->where('shops.is_selling', true)
+            ->where('products.is_selling', true)
+            ->select(
+                'products.id as id',
+                'products.name as name',
+                'products.price' ,
+                'products.sort_order as sort_order',
+                'products.information',
+                'secondary_categories.name as category' ,
+                'image1.filename as filename'
+            );
     }
 }
